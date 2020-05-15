0c1c8185c4c8ce13db74a2321f3237732a61ebab
chitragarj <jayateerthc@gmail.com>
2020-05-15 10:03:33 +0530

    Commit

---
+++ src/main/java/com/angeluar/Angeluar/AngeluarApplication.java
@@ -0,0 +1,13 @@
+package com.angeluar.Angeluar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngeluarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngeluarApplication.class, args);
+	}
+
+}
